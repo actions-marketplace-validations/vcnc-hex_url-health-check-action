ec8889871192f5edf49d0ff91d639855fc11fb63
seveibar <seveibar@gmail.com>
2021-08-26 14:38:17 -0700

    switch to axios, accept any error, add maxTime

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-const core = require("@actions/core");
-const proc = require("child_process");
-const duration = require("duration-js");
+const core = require("@actions/core")
+const proc = require("child_process")
+const duration = require("duration-js")
+const axios = require("axios")
 
 function asBoolean(str) {
-    return str && ["yes", "true", "y", "1"].includes(str);
+  return str && ["yes", "true", "y", "1"].includes(str)
 }
 
 const processConfig = {
-    stdio: "inherit",
-    encoding: "utf8",
-};
+  stdio: "inherit",
+  encoding: "utf8",
+}
 
-function curl(url, { maxAttempts, retryDelaySeconds, retryAll, followRedirect }) {
-    let retrySettings = "";
-    if (maxAttempts > 1) {
-        retrySettings = `--retry ${maxAttempts} --retry-delay ${retryDelaySeconds} --retry-connrefused`;
+async function curl(
+  url,
+  { maxAttempts, retryDelaySeconds, retryAll, followRedirect }
+) {
+  let success
+  for (let i = 0; i < maxAttempts; i++) {
+    core.info(`Checking ${url}`)
+    const res = await axios
+      .get(url, { timeout: requestTimeout })
+      .catch((e) => e)
+    if (res.success) {
+      core.info("Got success response")
+      success = true
+      break
+    } else {
+      core.info(`Received: ${JSON.stringify(res?.data || res?.response?.data)}`)
     }
-    let redirectSettings = followRedirect ? '-L' : '';
-    let retryAllSettings = retryAll ? '--retry-all-errors' : '';
-
-    core.info(`Checking ${url}`);
-    let command = `curl --fail -sv ${redirectSettings} ${url} ${retrySettings} ${retryAllSettings}`;
-    core.debug("Command: " + command);
-
-    let out = proc.execSync(command, processConfig);
-
-    core.info(out);
+    // Wait 100ms
+    await new Promise((resolve) => setTimeout(resolve, 100))
+  }
+  if (!success) {
+    throw new Error(JSON.stringify(res.data || res.response.data))
+  }
 }
 
 try {
-    let urlString = core.getInput("url", {required: true});
-    let maxAttemptsString = core.getInput("max-attempts");
-    let retryDelay = core.getInput("retry-delay");
-    let followRedirectString = core.getInput("follow-redirect");
-    let retryAllString = core.getInput("retry-all");
+  let urlString = core.getInput("url", { required: true })
+  let maxAttemptsString = core.getInput("max-attempts")
+  let maxTimeString = core.getInput("max-time")
+  let requestTimeoutString = core.getInput("request-timeout")
 
-    let urls = urlString.split("|");
-    let retryDelaySeconds = duration.parse(retryDelay).seconds();
-    let maxAttempts = parseInt(maxAttemptsString);
-    let followRedirect = asBoolean(followRedirectString);
-    let retryAll = asBoolean(retryAllString);
+  let urls = urlString.split("|")
+  let maxAttempts = parseInt(maxAttemptsString)
+  let maxTime = duration.parse(maxTimeString).milliseconds()
+  let requestTimeout = duration.parse(requestTimeoutString).milliseconds()
 
-    urls.forEach(url => {
-        curl(url, {maxAttempts, retryDelaySeconds, retryAll, followRedirect})
-    });
+  urls.forEach((url) => {
+    curl(url, { maxAttempts, maxTime, requestTimeout })
+  })
 
-    core.info("Success");
+  core.info("Success")
 } catch (e) {
-    console.error("Error running action", e);
-    core.setFailed(e.message);
+  console.error("Error running action", e)
+  core.setFailed(e.message)
 }
